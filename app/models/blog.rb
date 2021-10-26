0a5ebddf9ea24b2d2ff4d2c422040e67e70e1ef9
AndresLeiva93 <89427059+AndresLeiva93@users.noreply.github.com>
2021-10-26 01:06:54 -0300

    creación de validaciones

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,3 +1,4 @@
 class Blog < ApplicationRecord
     has_many :post
+    validates :name, :description, :presence => true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :blog
   has_many :message
+  validates :title, :content, :presence => true
+  validates :title, length: {minimum: 7}
 end
